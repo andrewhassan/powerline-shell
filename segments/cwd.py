272bf78dcfb715d008a59f238781ec86d65731de
Andrew Hassan <andrewihassan@gmail.com>
2013-07-11 12:58:29 -0400

    Fixed changes

--- segments/cwd.py
+++ segments/cwd.py
@@ -1,6 +1,5 @@
 import os
 
-# Andrew Hassan specific code here
 def get_short_path(cwd):
     result = []
     home = os.getenv('HOME')
@@ -10,20 +9,15 @@ def get_short_path(cwd):
     for i in range(len(names)):
         path += os.sep + names[i]
         if os.path.samefile(path, home):
-            result = ['~'] + names[i+1:]
-    result = names
-
-    if len(result) > 3:
-        result = [result[0]] + ["..."] + result[-2:]
-    
-    return result
+            return ['~'] + names[i+1:]
+    return names
 
 
 def add_cwd_segment():
     cwd = powerline.cwd or os.getenv('PWD')
     names = get_short_path(cwd.decode('utf-8'))
 
-    max_depth = powerline.args.cwd_max_depth
+    max_depth = 3
     if len(names) > max_depth:
         names = names[:2] + [u'\u2026'] + names[2 - max_depth:]
 
